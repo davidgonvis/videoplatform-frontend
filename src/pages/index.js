72445326bd005af9efac14eed4ab36c84d08a1b6
David <galvisd153@gmail.com>
2021-07-10 09:34:17 +0800

    feat: project tailwind base style configuration

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="test w-24 h-8 mb-4 bg-primary font-main">adsfdsa</div>
+      <div className="test w-24 h-8 mb-4 bg-secondary font-title">adsfdsa</div>
+      <div className="test w-24 h-8 mb-4 bg-primary">adsfdsa</div>
+      <div className="test w-24 h-8 mb-4">adsfdsa</div>
+      <div className="test w-24 h-8 mb-4 bg-blue-700" style={{background: "#5454D4"}}>adsfdsa</div>
+      <div className="test w-24 h-8 mb-4" style={{background: "#5454D41A"}}>adsfdsa</div>
+      <div className="test w-24 h-8 mb-4" style={{background: "#F7F7FB"}}>adsfdsa</div>
+      <div className="test w-24 h-8 mb-4" style={{background: "#696871"}}>adsfdsa</div>
+      <div className="test w-24 h-8 mb-4" style={{background: "#19191B"}}>adsfdsa</div>
+      <div className="test w-24 h-8 mb-4" style={{background: "#696871"}}>adsfdsa</div>
+      <div className="test w-24 h-8 mb-4" style={{background: "#3B8590"}}>adsfdsa</div>
+      <div className="test w-24 h-8 mb-4" style={{background: "#FFFFFFB3"}}>adsfdsa</div>
+      <div className="test w-24 h-8 mb-4" style={{background: "#FFFFFFB3"}}>adsfdsa</div>
+
+      <div className="test"></div>
+      Homep age
+    </div>
+  )
+}
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3B8590",
+        secondary: "#173054",
+        "theme-gray": "#696871",
+        "font-dark": "#19191B",
+      },
+      fontFamily: {
+        main: ["Circular Std", "sans-serif"],
+        title: ["SF Pro Display", "sans-serif"]
+      }
+    },
   },
   variants: {
     extend: {},
